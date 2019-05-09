ce7845e0b0724b440858dd4d545cc645a9c16962
Davi Costa <50250514+DMCDavi@users.noreply.github.com>
2019-05-09 12:04:18 -0300

    Create var_globais.h

---
+++ var_globais.h
@@ -0,0 +1,16 @@
+#define TERRA 10
+#define NEVE 11
+#define AGUA 4
+#define CAMINHOHOR 3
+#define CAMINHOVER 1
+#define HORIZONTAL 2
+#define VERTICAL 7
+#define CANTINFESQ 8
+#define CANTINFDIR 9
+#define CANTSUPESQ 5
+#define CANTSUPDIR 6
+#define PAIS 0
+
+char pais[21][4];
+
+char num_player[2], letra_pais[3]; //num_player armazenará os numeros 1 ou 2 referentes a cada player; letra_pais armazenará as letras que representarão cada país
