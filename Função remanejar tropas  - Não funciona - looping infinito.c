e27fc3f6f2293754d188b012c6d1e2dbcad0ac3b
Joseviniciusrs <50296699+Joseviniciusrs@users.noreply.github.com>
2019-05-27 10:37:16 -0300

    Update Função remanejar tropas  - Não funciona - looping infinito.c

--- Função remanejar tropas  - Não funciona - looping infinito.c
+++ Função remanejar tropas  - Não funciona - looping infinito.c
@@ -16,72 +16,60 @@ void remanejar_tropas(){
 	char resp[4];
 	
 	//iniciar o remanejamento
-	printf("Deseja remanejar? [sim][nao]");
-	scanf("%[^\n]s",resp);
+	fflush(stdin);
+	printf("Deseja remanejar? [sim][nao]\n");
+	scanf("%s",resp);
+	fflush(stdin);
 	
-	//loopin pra continuar a remanejar
-	while(strcmp(resp,"sim") == 0 || strcmp(resp,"SIM") == 0 || strcmp(resp,"Sim") == 0 strcmp(resp,"SIm") == 0 strcmp(resp,"sIM") == 0{
-		
+	//loopin pra continuar a remaneja
+	while(strcmp(resp,"sim") == 0 || strcmp(resp,"SIM") == 0 || strcmp(resp,"Sim") == 0 || strcmp(resp,"SIm") == 0 || strcmp(resp,"sIM") == 0){
+		fflush(stdin);
 		printf("Digite a letra do pais que deseja a saida de tropas:\n");
 		scanf("%s", &posicao_pais1); 
 		fflush(stdin);
 		
 		//VERFICA EXISTENCIA PAIS
-		while(!teste_existencia_pais(posicao_pais1) || teste_dono_pais(posicao_pais1)){
-			printf("[ERRO] Digite a letra do pais que deseja a saida de tropas:\n");
+		while(!teste_existencia_pais(posicao_pais1) || !teste_dono_pais(posicao_pais1)){
+			fflush(stdin);
+			printf("[ERRO - O PAIS NAO EXISTE OU NAO TE PERTENCE]\n");
+			printf("Digite a letra do pais que deseja a saida de tropas:\n");
 			scanf("%s", &posicao_pais1); 
 			fflush(stdin);
 		}
 		
-		printf("Digite a letra do pais que deseja a entrada de tropas:\n");
+		printf("%s,Digite a letra do pais que deseja a entrada de tropas:\n", player[vez]);
 		scanf("%s", &posicao_pais2);
 		 
 		//VERFICA EXISTENCIA PAIS		
-		while(!teste_existencia_pais(posicao_pais2) || teste_dono_pais(posicao_pais2)){
-			printf("[ERRO] Digite a letra do pais que deseja a saida de tropas:\n");
+		while(!teste_existencia_pais(posicao_pais2) || !teste_dono_pais(posicao_pais2)){
+			fflush(stdin);
+			printf("[ERRO - O PAIS NAO EXISTE OU NAO TE PERTENCE]\n");
+			printf("Digite a letra do pais que deseja a entrada de tropas:\n");
 			scanf("%s", &posicao_pais2); 
 			fflush(stdin);
 		}
 		
-		//insire a quantidade de tropas pra remanejar
-		int qntdTropasRemanejar =0;
-		printf("insira a quantidade de tropas que voce quer remanejar");
-		scanf("%d", &qntdTropasRemanejar);
-			
-		//POPULAR NUM_PAIS
-		if(posicao_pais1 >= 65 && posicao_pais1 <= 84 ){ //se a letra digitada estiver entre A e T maiusculas, a variavel num_pais vai armazanar um numero entre 0 e 19 que serÃƒÆ’Ã‚Â¡ usada posteiormente para representar as tropas de cada pais no vetor tropas_pais[21]
-			
-			num_pais1 = posicao_pais1 - 65;
-			
-		} else if(posicao_pais1 >= 97 && posicao_pais1 <= 116){ //se a letra digitada estiver entre a e t minusculas, a variavel num_pais vai armazanar um numero entre 0 e 19 que serÃƒÆ’Ã‚Â¡ usada posteiormente para representar as tropas de cada pais no vetor tropas_pais[21]
-			
-			num_pais1 = posicao_pais1 - 97;
-			
-		}
-		
-		if(posicao_pais2 >= 65 && posicao_pais2 <= 84 ){ //se a letra digitada estiver entre A e T maiusculas, a variavel num_pais vai armazanar um numero entre 0 e 19 que serÃƒÆ’Ã‚Â¡ usada posteiormente para representar as tropas de cada pais no vetor tropas_pais[21]
-			
-			num_pais2 = posicao_pais2 - 65;
-			
-		} else if(posicao_pais2 >= 97 && posicao_pais2 <= 116){ //se a letra digitada estiver entre a e t minusculas, a variavel num_pais vai armazanar um numero entre 0 e 19 que serÃƒÆ’Ã‚Â¡ usada posteiormente para representar as tropas de cada pais no vetor tropas_pais[21]
-			
-			num_pais2 = posicao_pais2 - 97;
-			
-		}
-		
-		
-		//VERIFICAR SE ELE PODE FAZER O REMANEJAMENTO
-		
-		//PEGA QUANTIDADE DE TROPAS DO PAIS
-		
-		//REMANEJAR TROPAS
-		
-		
-		printf("Deseja remanejar mais? [sim][nao]");
-		scanf("%[^\n]s",resp);
-	}
-	
-	
-	
+		if(teste_vizinhanca_pais(posicao_pais1,posicao_pais2)){
+			//insire a quantidade de tropas pra remanejar
+			int qntdTropasRemanejar =0;
+			printf("insira a quantidade de tropas que voce quer remanejar\n");
+			scanf("%d", &qntdTropasRemanejar);
 				
-	}
+		
+			//VERIFICAR SE ELE PODE FAZER O REMANEJAMENTO
+			
+			//PEGA QUANTIDADE DE TROPAS DO PAIS
+			
+			//REMANEJAR TROPAS	
+		}else{
+			printf("[ERRO - OS PAISES SELECIONADOS NAO PODEM FAZER DESLOCAMENTO]\n");
+			continue;
+		}
+		
+		
+		fflush(stdin);
+		printf("Deseja remanejar mais? [sim][nao]\n");
+		scanf("%s",resp);
+		fflush(stdin);
+	}		
+}
