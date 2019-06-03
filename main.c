6a610c42d316877a8f72a331675f071175166371
Joseviniciusrs <50296699+Joseviniciusrs@users.noreply.github.com>
2019-06-03 10:19:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,22 @@
 
 int main(){
 	
+	int menu,cont;
+	
+	printf("Digite 0 para as regras ou digite qualquer outro numero para jogar:\n");
+	
+	scanf("%d",&menu);
+	
+		if(menu == 0){
+		// as regras vao entrar aqui, colocarei-as assim que eu tiver tempo e principalmente paciencia :)
+			printf("regras vou botar depois\n");
+			system("pause");
+			system("cls");
+			menu = 1;	
+		}	
+	
+	if(menu != 0){
+	
 	cadastro_players();
 	
 	aleat_players();
@@ -18,13 +34,15 @@ int main(){
 	
 	system("pause");
 	
-	for(vez = 0; vez <= 1; vez++){ //laÃ§o de repetiÃ§Ã£o que faz cada jogador adicionar suas tropas no inicio do jogo
+	for(vez = 0; vez <= 1; vez++){ //laÃƒÂ§o de repetiÃƒÂ§ÃƒÂ£o que faz cada jogador adicionar suas tropas no inicio do jogo
 		
 		add_tropas();
 		
-		ataque_defesa(); //funcao bugada para sair do while
-		
 	}
+	
+	ataque_defesa(); //funcao bugada para sair do while
+	
+	} 
 	
 	return 0;
 	
