7f0dc0d966e900e23afa68edadd0fca7cd5a985a
Davi Costa <50250514+DMCDavi@users.noreply.github.com>
2019-06-03 10:33:35 -0300

    Update global.h

--- global.h
+++ global.h
@@ -1,4 +1,4 @@
-//Essa biblioteca contém as bibliotecas essenciais, as constantes e as variáveis globais
+//Essa biblioteca contÃ©m as bibliotecas essenciais, as constantes e as variÃ¡veis globais
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <windows.h>
+
 
 #define AUX 13
 #define TERRA 10
@@ -21,15 +22,15 @@
 #define CANTSUPDIR 6
 #define PAIS 0
 
-char pais[21][4]; //variável que será usada na função nome_paises para receber a nomeação de cada país
+char pais[21][4]; //variÃ¡vel que serÃ¡ usada na funÃ§Ã£o nome_paises para receber a nomeaÃ§Ã£o de cada paÃ­s
 
-char num_player[2], letra_pais[3]; //num_player armazenará os numeros 1 ou 2 referentes a cada player; letra_pais armazenará as letras que representarão cada país
+char num_player[2], letra_pais[3]; //num_player armazenarÃ¡ os numeros 1 ou 2 referentes a cada player; letra_pais armazenarÃ¡ as letras que representarÃ£o cada paÃ­s
 
-char player[3][100], player1[100], player2[100]; //variáveis que receberão os nicks dos players; player[0] vai receber o jogador que iniciará o jogo e vez_player[1] vai receber o player que não vai iniciar o jogo
+char player[3][100], player1[100], player2[100]; //variÃ¡veis que receberÃ£o os nicks dos players; player[0] vai receber o jogador que iniciarÃ¡ o jogo e vez_player[1] vai receber o player que nÃ£o vai iniciar o jogo
 
-int vez = 0; //armazenará os valores 0 ou 1, que servirão como parâmetro pra saber de quem é a vez na rodada, onde 0 representa o player que iniciou o jogo e 1 o outro player
+int vez = 0; //armazenarÃ¡ os valores 0 ou 1, que servirÃ£o como parÃ¢metro pra saber de quem Ã© a vez na rodada, onde 0 representa o player que iniciou o jogo e 1 o outro player
 
-int tropas_pais[21] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; //tropas_pais armazena a quantidade de tropas de cada país, onde cada valor do vetor representa um pais
+int tropas_pais[21] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; //tropas_pais armazena a quantidade de tropas de cada paÃ­s, onde cada valor do vetor representa um pais
 
 unsigned int posicao_pais_encontrado_ataque = 0, posicao_pais_encontrado_defesa = 0;
 
