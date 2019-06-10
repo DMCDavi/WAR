5749fa2884fcc3205682d53960a3414813c1ece7
Joseviniciusrs <50296699+Joseviniciusrs@users.noreply.github.com>
2019-06-10 20:12:27 -0300

    Update inicio.h

--- inicio.h
+++ inicio.h
@@ -1,11 +1,11 @@
-//Essa biblioteca contÃƒÂ©m as funÃƒÂ§ÃƒÂµes que serÃƒÂ£o usadas apenas no inicio do jogo
+//Essa biblioteca contÃƒÆ’Ã‚Â©m as funÃƒÆ’Ã‚Â§ÃƒÆ’Ã‚Âµes que serÃƒÆ’Ã‚Â£o usadas apenas no inicio do jogo
 
 /*int EsperarTempo(int i)
 {
 
 	for(i; i >= 0; i--)
 	{
-		printf("%i\n",i); // Escreve na tela o nÃºmero atual da 'contagem'
+		printf("%i\n",i); // Escreve na tela o nÃƒÂºmero atual da 'contagem'
 		//Delay(i*1000); // Espera 1 segundo
 	}
 }*/
@@ -16,6 +16,15 @@ int Menu_inicial(){
 	printf("\t\t\t\t\t\t\t\t\t   (1) - jogar \n");
 	printf("\t\t\t\t\t\t\t\t\t    :");
 	scanf("%d",&menu);
+	if(menu == 0){
+		printf("\n\t\t\t\t\t\t\t\t\t REGRAS: \t\t\t\t\t\t\t\t\t\n");
+		printf("\n 1 - O jogo possui 20 paises. Cada jogador comecara inicialmente com a quantidade de paises no total dividido por 2(ou seja, 10). \n");
+		printf("\n 2 - Todo turno, o jogador da rodada tem o direito de adicionar tropas em seus paises. A quantidade de tropas a adicionar eh igual ao numero de paises em posse do jogador dividido por 2!(arredondado para baixo.) \n");
+		printf("\n 3 - O jogador da vez tem o direito de atacar o oponente ou nao, a depender de sua vontade! \n");
+		printf("\n 4 - Ao final da parte de ataque, o jogador podera remanejar tropas ou nao, para ter como atacar com mais confianca no proximo turno ou se proteger do ataque do inimigo! \n");
+		printf("\n 5 - Por ser uma mini versao do jogo, o unico objetivo eh conquistar TODOS os territorios existentes! \n");
+		printf("\n 6 - Boa sorte e que venca o melhor!!! \n");
+	}
 /*	if(menu != int){
 		printf("");
 		Menu_inicial();
@@ -23,7 +32,7 @@ int Menu_inicial(){
 	return menu;
 }
 
-void cadastro_players(){ //funÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â§ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â£o que irÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¡ cadastrar os players em nomes de usuario
+void cadastro_players(){ //funÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â§ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â£o que irÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â¡ cadastrar os players em nomes de usuario
 	system("cls");
 	printf("\t\t\t\t\t\t\t\t Bem-vindo ao MINI WAR! \\o/ \\o/ \\o/   \n\n");
 	Menu_inicial();
@@ -71,15 +80,15 @@ void cadastro_players(){ //funÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â§ÃƒÆ�
 	}
 	
 }
-void aleat_players(){ //funÃ§Ã£o que define de forma aleatoria quem vai iniciar o jogo
+void aleat_players(){ //funÃƒÂ§ÃƒÂ£o que define de forma aleatoria quem vai iniciar o jogo
 	
 	int num_aleatorio;
 	
 	srand(time(NULL));
 	
-	num_aleatorio = rand() % 2; //um nÃºmero aleatÃ³rio que receberÃ¡ 0 ou 1
+	num_aleatorio = rand() % 2; //um nÃƒÂºmero aleatÃƒÂ³rio que receberÃƒÂ¡ 0 ou 1
 	
-	if(num_aleatorio == 0){ //se o nÃºmero aleatÃ³rio for 0, o jogo vai iniciar pelo player 1
+	if(num_aleatorio == 0){ //se o nÃƒÂºmero aleatÃƒÂ³rio for 0, o jogo vai iniciar pelo player 1
 		
 		strcpy(player[0], player1); 
 		
@@ -95,41 +104,41 @@ void aleat_players(){ //funÃ§Ã£o que define de forma aleatoria quem vai inic
 	
 }
 
-void cria_paises(char pais[21][4]){ //funÃ§Ã£o que nomearÃ¡ os vinte paises com 3 caracteres cada, distribuindo-os aleatoriamente para cada jogador
+void cria_paises(char pais[21][4]){ //funÃƒÂ§ÃƒÂ£o que nomearÃƒÂ¡ os vinte paises com 3 caracteres cada, distribuindo-os aleatoriamente para cada jogador
 		
 	int cont_nome_pais, num_aleatorio, qntd_paises_player1 = 0, qntd_paises_player2 = 0;
 	
 	srand(time(NULL));
 	
-	for(cont_nome_pais = 0; cont_nome_pais < 20; cont_nome_pais++){ //repete 20 vezes a operaÃ§Ã£o pra nomear todos os 20 paÃ­ses
+	for(cont_nome_pais = 0; cont_nome_pais < 20; cont_nome_pais++){ //repete 20 vezes a operaÃƒÂ§ÃƒÂ£o pra nomear todos os 20 paÃƒÂ­ses
 		
-		num_aleatorio = rand() % 2; //o numero aleatorio receberÃ¡ um numero entre 0 e 1
+		num_aleatorio = rand() % 2; //o numero aleatorio receberÃƒÂ¡ um numero entre 0 e 1
 		
-		if((num_aleatorio == 0 && qntd_paises_player1 < 10) || qntd_paises_player2 >= 10){ //se o numero aletorio receber 0 e o player 1 ainda nÃ£o tiver 10 paises sob seu controle ou se a quantidade de paises sob controle do player 2 tiver chegado a 10, o paÃ­s passarÃ¡ a ser do player 1
+		if((num_aleatorio == 0 && qntd_paises_player1 < 10) || qntd_paises_player2 >= 10){ //se o numero aletorio receber 0 e o player 1 ainda nÃƒÂ£o tiver 10 paises sob seu controle ou se a quantidade de paises sob controle do player 2 tiver chegado a 10, o paÃƒÂ­s passarÃƒÂ¡ a ser do player 1
 			
-			strcpy(num_player, "1\0"); //copia 1 na variÃ¡vel num_player
+			strcpy(num_player, "1\0"); //copia 1 na variÃƒÂ¡vel num_player
 			
 			qntd_paises_player1++; //aumenta em 1 a quantidade de paises sob controle do player 1
 			
-		} else if((num_aleatorio == 1 && qntd_paises_player2 < 10) || qntd_paises_player1 >= 10){ //se o numero aletorio receber 1 e o player 2 ainda nÃ£o tiver 10 paises sob seu controle ou se a quantidade de paises sob controle do player 1 tiver chegado a 10, o paÃ­s passarÃ¡ a ser do player 2
+		} else if((num_aleatorio == 1 && qntd_paises_player2 < 10) || qntd_paises_player1 >= 10){ //se o numero aletorio receber 1 e o player 2 ainda nÃƒÂ£o tiver 10 paises sob seu controle ou se a quantidade de paises sob controle do player 1 tiver chegado a 10, o paÃƒÂ­s passarÃƒÂ¡ a ser do player 2
 		
 			strcpy(num_player, "2\0"); //copia 2 na variavel num_player
 			
 			qntd_paises_player2++; //aumenta em 1 a quantidade de paises sob controle do player 2
 			
-		} letra_pais[0] = 65 + cont_nome_pais; //define a letra que cada paÃ­s vai receber, seguindo a tabela ASCII
+		} letra_pais[0] = 65 + cont_nome_pais; //define a letra que cada paÃƒÂ­s vai receber, seguindo a tabela ASCII
 		
-		letra_pais[1] = '-'; //adiciona um hÃ­fen que separa a identificaÃ§Ã£o do paÃ­s das tropas
+		letra_pais[1] = '-'; //adiciona um hÃƒÂ­fen que separa a identificaÃƒÂ§ÃƒÂ£o do paÃƒÂ­s das tropas
 		
-		letra_pais[2] = '\0'; //pÃµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
+		letra_pais[2] = '\0'; //pÃƒÂµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
 		
-		strcat(num_player, letra_pais); //concatena a numeraÃ§Ã£o do player com a letra do paÃ­s e o hÃ­fen na variÃ¡vel num_player
+		strcat(num_player, letra_pais); //concatena a numeraÃƒÂ§ÃƒÂ£o do player com a letra do paÃƒÂ­s e o hÃƒÂ­fen na variÃƒÂ¡vel num_player
 		
-		strcpy(pais[cont_nome_pais], num_player); //copia na matriz pais[21][4] a identificaÃ§Ã£o de cada paÃ­s e a qual player pertence 
+		strcpy(pais[cont_nome_pais], num_player); //copia na matriz pais[21][4] a identificaÃƒÂ§ÃƒÂ£o de cada paÃƒÂ­s e a qual player pertence 
 		
-		num_player[0] = '\0'; //pÃµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
+		num_player[0] = '\0'; //pÃƒÂµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
 		
-		letra_pais[0] = '\0'; //pÃµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
+		letra_pais[0] = '\0'; //pÃƒÂµe um fim na string (nn sei qual a necessidade disso, mas se tirar buga)
 	}
 	
 }
